50a23e00421e42b29a47a11f9a4dcb2ea4eeefd8
James Golick <jamesgolick@gmail.com>
2014-06-01 21:10:03 -0400

    Fix the gem requirements.

--- test/fixtures/cookbooks/packagecloud_test/recipes/rubygems.rb
+++ test/fixtures/cookbooks/packagecloud_test/recipes/rubygems.rb
@@ -5,4 +5,6 @@ packagecloud_repo "computology/packagecloud-cookbook-test-public" do
   type "gem"
 end
 
-gem_package "jakedotrb"
+gem_package "jakedotrb" do
+  version "0.0.1"
+end
--- test/fixtures/cookbooks/packagecloud_test/recipes/rubygems_private.rb
+++ test/fixtures/cookbooks/packagecloud_test/recipes/rubygems_private.rb
@@ -1,3 +1,6 @@
+execute "update apt" do
+  command "apt-get update"
+end
 package "ruby"
 package "rubygems"
 
@@ -6,4 +9,6 @@ packagecloud_repo "computology/packagecloud-cookbook-test-private" do
   master_token "762748f7ae0bfdb086dd539575bdc8cffdca78c6a9af0db9"
 end
 
-gem_package "jakedotrb"
+gem_package "jakedotrb" do
+  version "0.0.1"
+end
