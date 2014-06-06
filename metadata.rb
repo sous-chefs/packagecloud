0759c13a54107c8b0f3b1790c18773ac5cc86692
James Golick <jamesgolick@gmail.com>
2014-06-05 23:02:11 -0400

    Change ver.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'james@packagecloud.io'
 license          'All rights reserved'
 description      'Installs/Configures packagecloud.io repositories.'
 long_description 'Installs/Configures packagecloud.io repositories.'
-version          '0.0.1'
+version          '0.0.0'
 
 depends "apt"
 depends "yum"
