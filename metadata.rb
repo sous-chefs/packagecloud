a8fb8ef813c3b2acb5d59b4bd11a6d8035a104d3
Joe Damato <ice799@gmail.com>
2014-10-10 13:56:56 -0700

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'joe@packagecloud.io'
 license 'Apache 2.0'
 description 'Installs/Configures packagecloud.io repositories.'
 long_description 'Installs/Configures packagecloud.io repositories.'
-version '0.0.12'
+version '0.0.13'
 
 depends 'apt'
