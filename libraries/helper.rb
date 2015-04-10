65d01114a5fe55c7713e2bceb217906a2d7d0a06
Salim Afiune <afiune@chef.io>
2015-04-10 16:30:38 -0700

    Proxy Implementation
    As an Enterprise User I expect to be able to use my proxy to configure package cloud repos and pull down open source packages.

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -3,16 +3,11 @@ require 'net/https'
 module PackageCloud
   module Helper
     def get(uri, params)
-      uri.query     = URI.encode_www_form(params)
-      req           = Net::HTTP::Get.new(uri.request_uri)
-
-      req.basic_auth uri.user, uri.password if uri.user
-
-      http = Net::HTTP.new(uri.hostname, uri.port)
-      http.use_ssl = true
-
-      resp = http.start { |h| h.request(req) }
-
+      uri.query = URI.encode_www_form(params)
+      req       = Net::HTTP::Get.new(uri.request_uri)
+      
+      resp = http_request(req)
+      
       case resp
       when Net::HTTPSuccess
         resp
@@ -26,18 +21,34 @@ module PackageCloud
       req.form_data = params
 
       req.basic_auth uri.user, uri.password if uri.user
-
-      http = Net::HTTP.new(uri.hostname, uri.port)
-      http.use_ssl = true
-
-      resp = http.start { |h|  h.request(req) }
-
+      
+      resp = http_request(req)
+      
       case resp
       when Net::HTTPSuccess
         resp
       else
         raise resp.inspect
       end
+    end
+    
+    def http_request(request)
+      if ENV['https_proxy'] || ENV['http_proxy']
+        proxy_url = ENV['https_proxy'] || ENV['http_proxy']
+        proxy_uri = URI.parse(proxy)
+        proxy     = Net::HTTP::Proxy(proxy_uri.host, proxy_uri.port, proxy_uri.user, proxy_uri.password)
+
+        response = proxy.start(uri.host, :use_ssl => proxy_uri.scheme == 'https') do |http|
+          http.request(request)
+        end
+      else
+        http = Net::HTTP.new(uri.hostname, uri.port)
+        http.use_ssl = true
+  
+        response = http.start { |h|  h.request(request) }
+      end
+      
+      response
     end
   end
 end
