189d73b918de682d637d4cbc5d1bf9001fcdb3b1
Tim Smith <tsmith@chef.io>
2017-04-26 12:32:27 -0700

    Define the matcher for Chefspec
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,7 +1,7 @@
 if defined?(ChefSpec)
+  ChefSpec.define_matcher :packagecloud_repo
 
   def create_packagecloud_repo(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:packagecloud_repo, :add, resource_name)
   end
-
 end
