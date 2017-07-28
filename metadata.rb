42bbf642bfc09ae698bfa9b7a9f8f1d83241d8ea
Aaron Kalin <akalin@martinisoftware.com>
2017-07-28 14:14:36 -0500

    Update and cleanup metadata
    This adds more Chef 12/13 compatibility

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,15 @@
 name 'packagecloud'
 maintainer 'Joe Damato'
 maintainer_email 'joe@packagecloud.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs/Configures packagecloud.io repositories.'
 long_description 'Installs/Configures packagecloud.io repositories.'
+source_url 'https://github.com/computology/packagecloud-cookbook'
+issues_url 'https://github.com/computology/packagecloud-cookbook/issues'
 version '0.3.0'
-source_url 'https://github.com/computology/packagecloud-cookbook' if respond_to?(:source_url)
-issues_url 'https://github.com/computology/packagecloud-cookbook/issues' if respond_to?(:issues_url)
+
+chef_version '>= 12.6'
+
+%w( amazon centos fedora debian redhat suse opensuse ubuntu).each do |os|
+  supports os
+end
