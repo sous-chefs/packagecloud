82c7a9a2bee1f19f2cb72a370be89d885ab9b0e6
Tim Smith <tsmith@chef.io>
2017-08-02 11:11:59 -0700

    Install EPEL on centos 5 test nodes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/packagecloud_test/recipes/distro_deps.rb
+++ test/fixtures/cookbooks/packagecloud_test/recipes/distro_deps.rb
@@ -1,5 +1,13 @@
 case node['platform_family']
 when 'rhel', 'fedora', 'amazon'
+  yum_repository 'epel5' do
+    mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-5&arch=$basearch'
+    description 'Extra Packages for Enterprise Linux 5 - $basearch'
+    enabled true
+    gpgcheck true
+    gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL'
+    only_if { node['platform_version'].to_i == 5 }
+  end
   package 'ruby'
   package 'rubygems'
 when 'debian'
