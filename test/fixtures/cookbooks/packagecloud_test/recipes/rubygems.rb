0e92c8e47d67c1d5a6f89ef3946f94edd9e2d029
Tim Smith <tsmith@chef.io>
2018-03-09 17:08:30 -0800

    Apply the proposed testing patch
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/packagecloud_test/recipes/rubygems.rb
+++ test/fixtures/cookbooks/packagecloud_test/recipes/rubygems.rb
@@ -1,13 +1,17 @@
-return if platform?('centos') && node['platform_version'].to_i == 5
-
 include_recipe 'packagecloud_test::distro_deps'
 
 packagecloud_repo 'computology/packagecloud-cookbook-test-public' do
   type 'gem'
 end
 
-gem_package 'jakedotrb' do
-  options '--bindir /usr/local/bin'
-  version '0.0.1'
-  source 'https://packagecloud.io/computology/packagecloud-cookbook-test-public/'
+if platform?('centos') && node['platform_version'].to_i == 5
+  execute 'gem install' do
+    command 'gem install jakedotrb --bindir /usr/local/bin'
+  end
+else
+  gem_package 'jakedotrb' do
+    options '--bindir /usr/local/bin'
+    version '0.0.1'
+    source 'https://packagecloud.io/computology/packagecloud-cookbook-test-public/'
+  end
 end
--- test/fixtures/cookbooks/packagecloud_test/recipes/rubygems_private.rb
+++ test/fixtures/cookbooks/packagecloud_test/recipes/rubygems_private.rb
@@ -1,5 +1,3 @@
-return if platform?('centos') && node['platform_version'].to_i == 5
-
 include_recipe 'packagecloud_test::distro_deps'
 
 packagecloud_repo 'computology/packagecloud-cookbook-test-private' do
@@ -7,7 +5,13 @@ packagecloud_repo 'computology/packagecloud-cookbook-test-private' do
   master_token '762748f7ae0bfdb086dd539575bdc8cffdca78c6a9af0db9'
 end
 
-gem_package 'jakedotrb' do
-  options '--bindir /usr/local/bin'
-  version '0.0.1'
+if platform?('centos') && node['platform_version'].to_i == 5
+  execute 'gem install' do
+    command 'gem install jakedotrb --bindir /usr/local/bin'
+  end
+else
+  gem_package 'jakedotrb' do
+    options '--bindir /usr/local/bin'
+    version '0.0.1'
+  end
 end
