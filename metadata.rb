787615a84a488c12230f1398994a139e8159bcef
itjedi42 <44007102+itjedi42@users.noreply.github.com>
2018-10-25 18:21:06 -0500

    Fix for users on Chef 11.10

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ license 'Apache-2.0'
 description 'Installs/Configures packagecloud.io repositories.'
 long_description 'Installs/Configures packagecloud.io repositories.'
 version '1.0.0'
-source_url 'https://github.com/computology/packagecloud-cookbook'
-issues_url 'https://github.com/computology/packagecloud-cookbook/issues'
+source_url 'https://github.com/computology/packagecloud-cookbook' if respond_to?(:source_url)
+issues_url 'https://github.com/computology/packagecloud-cookbook/issues' if respond_to?(:issues_url)
 chef_version '>= 12.5' if respond_to?(:chef_version)
 %w(ubuntu debian redhat centos amazon oracle fedora scientific).each do |p|
   supports p
