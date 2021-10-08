ce05704eba90a55a7a8ea0809d016346cb0a59b8
Rajesh Paul <rajesh.paul@progress.com>
2021-10-08 17:11:16 +0530

    chef version support changed from 12.5+ to 15.3+
    Signed-off-by: Rajesh Paul <rajesh.paul@progress.com>

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description 'Installs/Configures packagecloud.io repositories.'
 version '1.0.1'
 source_url 'https://github.com/computology/packagecloud-cookbook' if respond_to?(:source_url)
 issues_url 'https://github.com/computology/packagecloud-cookbook/issues' if respond_to?(:issues_url)
-chef_version '>= 12.5' if respond_to?(:chef_version)
+chef_version '>= 15.3' if respond_to?(:chef_version)
 %w(ubuntu debian redhat centos amazon oracle fedora scientific).each do |p|
   supports p
 end
