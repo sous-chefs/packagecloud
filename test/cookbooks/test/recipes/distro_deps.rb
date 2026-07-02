848b9bdf78b8f60e139b5a0e1af905eba564542f
Dan Webb <dan.webb@damacus.io>
2026-07-02 06:57:32 +0100

    fix: avoid dnf repo refresh for test deps

--- test/cookbooks/test/recipes/distro_deps.rb
+++ test/cookbooks/test/recipes/distro_deps.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
-package 'ca-certificates'
+package 'ca-certificates' do
+  not_if 'rpm -q ca-certificates'
+  only_if { platform_family?('amazon', 'fedora', 'rhel') }
+end
+
+package 'ca-certificates' do
+  not_if { platform_family?('amazon', 'fedora', 'rhel') }
+end
 
 if platform_family?('debian')
   package %w(ruby dpkg-dev rubygems)
